8632f75c85013ed6d1767a9cb5bad30f44f756b3
Guti <gustavo.costanti.lara@gmail.com>
2021-03-23 14:28:28 -0300

    dev01

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,27 @@
+<h3>Fornecedor</h3>
+
+{{-- comentário --}}
+
+
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[1]['status'] }}
+    <br>
+    CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
+    <br>
+    Telefone: ({{ $fornecedores[1]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
+    @switch($fornecedores[1]['ddd'])
+        @case ('11')
+            São Paulo - SP
+            @break
+        @case ('32')
+            Juiz de Fora - MG
+            @break
+        @case ('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado.
+    @endswitch
+@endisset
